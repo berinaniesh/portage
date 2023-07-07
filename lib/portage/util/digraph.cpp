3481db6143fb38ff4af7c4f78549b73919825e2c
Berin Aniesh <berinaniesh@gmail.com>
2023-07-07 15:19:41 +0530

    add to digraph

--- lib/portage/util/digraph.cpp
+++ lib/portage/util/digraph.cpp
@@ -1,7 +1,32 @@
 // Copyright 2023 Gentoo Foundation
 // Distributed under the terms of the GNU General Public License v2
 
-#define PY_SSIZE_T_CLEAN
-#include <Python.h>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
 
+class Digraph {
+public:
+  unordered_map<string, string> nodes;
+  vector<std::string> order;
 
+  void add(string a, string b) {
+    nodes[a] = b;
+    cout << "Inserted\n";
+  }
+  void pring() {
+    for (auto a : nodes) {
+      cout << a.first << a.second << std::endl;
+    }
+  }
+};
+
+int main() {
+  Digraph digraph;
+  string a = "sab";
+  string b = "sdf";
+  digraph.add(a, b);
+  digraph.pring();
+}
